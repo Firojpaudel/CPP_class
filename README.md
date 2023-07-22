fd817ee1bd634cf4fb7ffbe32e3e9c5763f5dac7
Firojpaudel <firojpaudel@gmail.com>
2023-07-22 18:06:01 +0545

    prime updated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# CPP_class
- classroom assignments 
+# C++ LAB Assignments
+ In this repo, I'll keep on updating every C++ codes that we learn in Second Semester of CSIT. 
+
---
+++ prime.cpp
@@ -0,0 +1,21 @@
+// WAP to check if given number is prime or not.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num, i;
+    cout<< "Enter a number: ";
+    cin >> num;
+    int count = 0;
+    for (i=0;i<=num;i++){
+        if (num%i==0){
+            count++;
+        }
+    }
+    if (count == 2){
+        cout<< num << "is a prime number 🙂";
+    }
+    else{
+        cout<< "Sorry! "<< num << "is not a prime number ☹️";
+    }
+}
