e9238ae98829bf1f353e2b1d1874dfd0b683633d
Firojpaudel <firojpaudel@gmail.com>
2023-07-27 10:59:04 +0545

    with address

---
+++ day3_passby_Reference.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class complex{
+    int real,imag;
+    public:
+    void input (){
+        cout<<"Enter real part: ";
+        cin>> real;
+        cout<<"Enter imaginary part: ";
+        cin>> imag;
+    }
+    void sum(complex &a, complex &b);
+    void display();
+};
+
+void complex:: sum(complex &a, complex &b)
+{
+    real= a.real+b.real;
+    imag= a.imag+b.imag;
+}
+
+void complex:: display(){
+    cout<<"The sum is: "<<real<<"+ i"<<imag<<endl;
+}
+
+int main(){
+    complex a1,a2,a3;
+    a1.input();
+    a2.input();
+    a3.sum(a1,a2);
+    a3.display();
+    return 0;
+}
