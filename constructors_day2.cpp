4482853a4e55435d11649374c37b3b82a81babc9
Firojpaudel <firojpaudel@gmail.com>
2023-08-03 10:43:54 +0545

    parameterized constructor updated

--- constructors_day2.cpp
+++ constructors_day2.cpp
@@ -1,10 +1,31 @@
+
+//Default constructor 
+// #include<iostream>
+// using namespace std;
+
+// class test{
+//     int x,y;
+//     public:
+//     test(){
+//         cout<<"Enter x and y";
+//         cin>>x>>y;
+//         cout<<x<<y;
+//     }
+// };
+
+// int main (){
+//     test t;
+// }
+
+
+//Parameterized constructor 
 #include<iostream>
 using namespace std;
 
 class test{
     int x,y;
     public:
-    test(){
+    test(int a,int b){
         cout<<"Enter x and y";
         cin>>x>>y;
         cout<<x<<y;
@@ -12,5 +33,8 @@ class test{
 };
 
 int main (){
-    test t;
+    int x,y;
+    // test (x,y);
+    //OR
+    test t = test(x,y);
 }
