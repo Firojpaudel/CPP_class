16db5d574fe3a15bd8b4ebfcff2e1b3de20fffb8
Firojpaudel <firojpaudel@gmail.com>
2023-08-03 11:19:07 +0545

    constuctor overloading

--- constructors_day2.cpp
+++ constructors_day2.cpp
@@ -39,26 +39,71 @@
 //     test t = test(x,y);
 // }
 
-//Copy constructor
- #include<iostream>
-using namespace std;
+// //Copy constructor
+//  #include<iostream>
+// using namespace std;
 
-class test{
-    int x;
-    public:
-    test(){
-        cout<<"Enter the value of x: ";
-        cin>>x;
-    }
-    test (test &t)
-    {
-        cout<<t.x;
+// class test{
+//     int x,y;
+//     public:
+//     test(){
+//         cout<<"Enter the value of x: ";
+//         cin>>x;
+//     }
+//     test (test &t)
+//     {
+//         cout<<t.x;
+//     }
+// };
+
+// int main (){
+//     test t1;
+//     test t2=t1;
+//     //OR
+//     // test t2(t1);
+// }
+
+//Constructor Overloading::
+#include <iostream>
+#include <string>
+
+class MyClass {
+private:
+    int num;
+    std::string name;
+
+public:
+    // Default Constructor
+    MyClass() : num(0), name("Default") {}
+
+    // Parameterized Constructor
+    MyClass(int number, const std::string& str) : num(number), name(str) {}
+
+    // Copy Constructor
+    MyClass(const MyClass& other) : num(other.num), name(other.name + "_copy") {}
+
+    // Display function
+    void display() const {
+        std::cout << "Number: " << num << ", Name: " << name << std::endl;
     }
 };
 
-int main (){
-    test t1;
-    test t2=t1;
-    //OR
-    // test t2(t1);
+int main() {
+    // Using Default Constructor
+    MyClass obj1;
+    std::cout << "Default Constructor Output: ";
+    obj1.display();
+
+    // Using Parameterized Constructor
+    MyClass obj2(42, "Parameterized");
+    std::cout << "Parameterized Constructor Output: ";
+    obj2.display();
+
+    // Using Copy Constructor
+    MyClass obj3 = obj2; // Alternatively, MyClass obj3(obj2);
+    std::cout << "Copy Constructor Output: ";
+    obj3.display();
+
+    return 0;
 }
+
