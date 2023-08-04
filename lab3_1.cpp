9ea08533edcbcf0fb6000d413cd2c7dc73e29d36
Firojpaudel <firojpaudel@gmail.com>
2023-08-04 11:06:28 +0545

    lab3

--- lab3_1.cpp
+++ lab3_1.cpp
@@ -2,8 +2,22 @@
 */
 
 #include<iostream>
+#include<string.h>
 using namespace std;
 
 class Programming{
-    
+    public:
+    Programming(){
+        cout<<"I love C++"<<endl;
+    }
+    Programming(string a){
+        cin>>a;
+        cout<<"I love "<<a<<endl;
+    }
 };
+
+int main (){
+    string a;
+    Programming x;
+    Programming y(a);
+}
