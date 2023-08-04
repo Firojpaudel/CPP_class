a613d60749b48e78655f07e265d273be4bc3ed5d
Firojpaudel <firojpaudel@gmail.com>
2023-08-04 10:45:38 +0545

    hey!

---
+++ lab3_1.cpp
@@ -0,0 +1,9 @@
+/*Create a class named 'Programming' while creating an object of a class, if nothing passed to it, then print message "I love C++". If some string is passed, then print the passed string in place of C++ ie print "I love (your passed string)".
+*/
+
+#include<iostream>
+using namespace std;
+
+class Programming{
+    
+};
