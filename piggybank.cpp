e3441c1a3597e9fbfa9749964cae7e9a4e25d502
Firojpaudel <firojpaudel@gmail.com>
2023-08-07 10:56:46 +0545

    hey !

---
+++ piggybank.cpp
@@ -0,0 +1,38 @@
+/*Create a piggie bank */
+#include<iostream>
+using namespace std;
+
+class addamount{
+    private:
+        int a;
+        int balance = 50;   // Initial balance is 50
+    public:
+        
+        addamount(){        // Default Constructor adding no amount to the Piggie Bank
+            
+        }
+        addamount(int a){   // Constructor to add the amount to existing balance
+            balance += a;   
+        }
+        void deposit(int a) {
+            if (a > 0) {
+                cout << a << " is added to the Piggy Bank" << endl;
+            } 
+            else {
+                cout << "No amount will be added to the Piggy Bank." << endl;
+            }
+        }
+        int total(){
+            return balance;
+        } 
+};
+int main(){
+    int a;
+    cout << "Enter the amount to be added in a Piggie Bank : ";
+    cin >> a;
+    addamount s1(a);        //Created an object with the input amount which calls the constructor
+    addamount s2 =s1;       //Created a copy of the object s1
+    s2.deposit(a);          // Deposits the input amount into the copied object
+    cout << "Total amount in Piggy Bank: " << s2.total();   //Displays the total balance
+    
+}
