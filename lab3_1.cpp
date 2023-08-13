c3c0376460c73ebc93fc6f3f792120792d277ae6
Firojpaudel <firojpaudel@gmail.com>
2023-08-13 15:23:41 +0545

    lab update

--- lab3_1.cpp
+++ lab3_1.cpp
@@ -1,23 +1,28 @@
 /*Create a class named 'Programming' while creating an object of a class, if nothing passed to it, then print message "I love C++". If some string is passed, then print the passed string in place of C++ ie print "I love (your passed string)".
 */
 
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string>
 using namespace std;
-
-class Programming{
-    public:
-    Programming(){
-        cout<<"I love C++"<<endl;
-    }
-    Programming(string a){
-        cin>>a;
-        cout<<"I love "<<a<<endl;
+    class Programming {
+        private:
+        string name;
+        public:
+        Programming() {
+            cout << "I love C++ " << endl;
+        }
+        Programming(string name) {
+        cout << "I love " << name;
     }
 };
-
-int main (){
-    string a;
-    Programming x;
-    Programming y(a);
+int main() {
+    string name;
+    cin >> name;
+    if (name.length() >1) {
+        Programming programming(name);
+    } 
+    else {
+        Programming ();
+    }
+    return 0;
 }
