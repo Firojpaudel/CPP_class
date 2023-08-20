2744932d87fbceca530f825dbdf23cbc5eeac050
Firojpaudel <firojpaudel@gmail.com>
2023-08-20 19:33:47 +0545

    conatenate with +

---
+++ overload+toconcatenatestrings.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class str{
+private:
+    string name;
+public:
+    str(){
+        name=" ";
+    }
+    str(string b){
+        name=b;
+    }
+    void getdata() {
+        cout << "Enter a string: ";
+        cin >> name;
+    }
+    void show(){
+        cout << name;
+    }
+    str operator +(str a){
+        str temp;
+        temp.name=name+a.name;
+        return temp;
+    }
+};
+int main(){
+    str a,b,c;
+    a.getdata();
+    b.getdata();
+    c=a+b;
+    c.show();
+    return 0;
+}
