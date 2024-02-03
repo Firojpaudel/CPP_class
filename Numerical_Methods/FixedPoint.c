2b5607e05eef28e273568ab19f3d1846ce463d5f
Firojpaudel <firojpaudel@gmail.com>
2024-02-03 20:36:36 +0545

    Added Numerical Methods :)

---
+++ Numerical_Methods/FixedPoint.c
@@ -0,0 +1,37 @@
+#include <stdio.h> //fixed point method
+#include <conio.h>
+int a1, a2, a3, a0;
+float f(float x)
+{
+    return a3 * x * x * x + a2 * x * x + a1 * x + a0;
+}
+float g(float x)
+{
+    return (-a0 - a3 * x * x * x - a2 * x * x) / a1;
+}
+int main()
+{
+    float x0, gx0, x1, E, Era;
+    int i = 1;
+    printf("Enter of constant of each power of variable :\n");
+    printf("In the order a3 a2 a1 a0\n");
+    scanf("%d%d%d%d", &a3, &a2, &a1, &a0);
+    printf("Enter initial guess and error precision:\n");
+    scanf("%f%f", &x0, &E);
+    while (1)
+    {
+        x1 = g(x0);
+        Era = fabs((x1 - x0) / x1);
+        if (Era >= E)
+        {
+            x0 = x1;
+            i++;
+        }
+        else
+        {
+            printf("The root is %f.\n", x1);
+            printf("No. of iteration = %d", i);
+            break;
+        }
+    }
+}
